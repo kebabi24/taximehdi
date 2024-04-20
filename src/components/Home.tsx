afd8e8c30f79a2d627c2c59863b282bf4748e5e4
Kebabi Abdelmounaim <kebabiabdel@gmail.com>
2024-04-20 20:09:30 +0100

    center

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -32,175 +32,160 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import SvgIcon from "@mui/material/SvgIcon";
 const Home = () => {
   const [type, setType] = React.useState("");
+  const [isShowMore1, setIsShowMore1] = useState(false);
+  const [isShowMore2, setIsShowMore2] = useState(false);
+  const [isShowMore3, setIsShowMore3] = useState(false);
+  const [isShowMore4, setIsShowMore4] = useState(false);
+  const [isShowMore5, setIsShowMore5] = useState(false);
+  const [isShowMore6, setIsShowMore6] = useState(false);
+  const [isShowMore7, setIsShowMore7] = useState(false);
+  const solutions = [
+    {
+      id: 1,
+      name: "QAD ERP",
+      // logo: qad,
+      description:
+        "la solution ERP QAD est un outil de gestion intégré de toute l’activité de l’entreprise, éditée par QAD USA, ce produit est mis en place en Algérie pour de grande entreprises industrielles dans divers secteurs d’activité, principalement l’agro-alimentaire, mais aussi la transformation de produit plastique, produit de bien-être, et dans l’électronique.  ",
+      state: isShowMore1,
+      toggle: (key: boolean) => toggleReadMoreLess1(key),
+    },
+  ];
+
+  const toggleReadMoreLess1 = (state: boolean) => {
+    setIsShowMore1(!state);
+  };
 
   const handleChange = (event: SelectChangeEvent) => {
     setType(event.target.value);
   };
   return (
-    <div className="h-[calc(120vh-5rem)] overflow-hidden">
-      <div className="bg-cover bg-[url('assets/hero-002.jpg')] flex bg-center bg-no-repeat h-full w-full container flex flex-wrap lg:px-12 px-5 max-w-screen-2xl py-24">
+    <div
+      className="md:px-14 px-4 py-16 max-w-screen-2xl mx-auto bg-[url('assets/hero-002.jpg')] overflow-hidden"
+      id="solutions"
+    >
+      <div className="text-center my-2"></div>
+      <div className="mt-14 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:w-11/12 mx-auto gap-12">
         <div
-          className="bg-white  lg:w-1/3 md:w-1/3"
-          style={{ borderRadius: 10 }}
+          style={{ borderRadius: "10px" }}
+          className="card  text-center  mx-auto md:h-100 rounded-md shadow cursor-pointer hover:-translate-y-5 hover:border-b-4 hover:border-indigo-700 transition-all duration-300 flex h-full bg-bgWhite justify-center items-center"
         >
-          <div
-            className="w-full bg-textPrimary mb-5"
-            style={{
-              display: "flex",
-              alignItems: "center",
-              borderStartStartRadius: 10,
-              borderStartEndRadius: 10,
-              height: "100px",
-            }}
-          >
-            <span className="px-10 font-bold">Order Now!</span>
-          </div>
-          <div
-            className="p-5"
-            style={{ display: "flex", flexDirection: "column" }}
-          >
-            <TextField
-              style={{ marginBottom: "7px" }}
-              id="input-with-icon-textfield"
-              placeholder="Localisation de départ"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LoginIcon />
-                  </InputAdornment>
-                ),
-              }}
-              variant="outlined"
-            />
-            <TextField
-              id="input-with-icon-textfield"
-              placeholder="Localisation d'arrivé"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LogoutIcon />
-                  </InputAdornment>
-                ),
-              }}
-              variant="outlined"
-            />
-
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={["MobileDateTimePicker"]}>
-                <DemoItem>
-                  <DesktopDateTimePicker
-                    defaultValue={dayjs("2022-04-17T15:30")}
-                    slotProps={{
-                      // Targets the `InputAdornment` component.
-                      inputAdornment: {
-                        position: "start",
-                      },
-                    }}
-                  />
-                </DemoItem>
-              </DemoContainer>
-            </LocalizationProvider>
-            <FormControl style={{ marginBottom: "7px", marginTop: "7px" }}>
-              <Select
-                value={type}
-                onChange={handleChange}
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-                renderValue={(value) => {
-                  console.log(value);
-                  return (
-                    <Box sx={{ display: "flex", gap: 1 }}>
-                      <SvgIcon>
-                        <LocalTaxiIcon />
-                      </SvgIcon>
-                      {value}
-                    </Box>
-                  );
-                }}
-              >
-                <MenuItem value=""></MenuItem>
-
-                <MenuItem value={"Simple"}>Simple</MenuItem>
-                <MenuItem value={"Confort"}>Confort</MenuItem>
-                <MenuItem value={"Van"}>Van</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField
-              style={{ marginBottom: "40px" }}
-              id="input-with-icon-textfield"
-              placeholder="Numéro de téléphone"
-              InputProps={{
-                inputMode: "tel",
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LocalPhoneIcon />
-                  </InputAdornment>
-                ),
-              }}
-              variant="outlined"
-            />
-            <Button
-              variant="contained"
-              size="large"
+          <div className=" h-50 md:w-[400px] mx-auto flex flex-col justify-center items-center ">
+            <div
+              className="w-full bg-textPrimary mb-5"
               style={{
-                backgroundColor: "#F09721",
-                color: "black",
-                fontWeight: "bold",
+                display: "flex",
+                alignItems: "center",
+
+                borderStartStartRadius: 10,
+                borderStartEndRadius: 10,
+                height: "100px",
               }}
             >
-              Réserver maintenant!
-            </Button>
+              <span
+                style={{ marginLeft: "28px", fontSize: "24px" }}
+                className="px-10 font-bold "
+              >
+                BOOK NOW!
+              </span>
+            </div>
+            <div
+              className="p-5"
+              style={{ display: "flex", flexDirection: "column" }}
+            >
+              <TextField
+                style={{ marginBottom: "7px" }}
+                id="input-with-icon-textfield"
+                placeholder="Localisation de départ"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LoginIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+              <TextField
+                id="input-with-icon-textfield"
+                placeholder="Localisation d'arrivé"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LogoutIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={["MobileDateTimePicker"]}>
+                  <DemoItem>
+                    <DesktopDateTimePicker
+                      defaultValue={dayjs("2022-04-17T15:30")}
+                      slotProps={{
+                        // Targets the `InputAdornment` component.
+                        inputAdornment: {
+                          position: "start",
+                        },
+                      }}
+                    />
+                  </DemoItem>
+                </DemoContainer>
+              </LocalizationProvider>
+              <FormControl style={{ marginBottom: "7px", marginTop: "7px" }}>
+                <Select
+                  value={type}
+                  onChange={handleChange}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                  renderValue={(value) => {
+                    console.log(value);
+                    return (
+                      <Box sx={{ display: "flex", gap: 1 }}>
+                        <SvgIcon>
+                          <LocalTaxiIcon />
+                        </SvgIcon>
+                        {value}
+                      </Box>
+                    );
+                  }}
+                >
+                  <MenuItem value=""></MenuItem>
+
+                  <MenuItem value={"Simple"}>Simple</MenuItem>
+                  <MenuItem value={"Confort"}>Confort</MenuItem>
+                  <MenuItem value={"Van"}>Van</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                style={{ marginBottom: "40px" }}
+                id="input-with-icon-textfield"
+                placeholder="Numéro de téléphone"
+                InputProps={{
+                  inputMode: "tel",
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LocalPhoneIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+              <Button
+                variant="contained"
+                size="large"
+                style={{
+                  backgroundColor: "#F09721",
+                  color: "black",
+                  fontWeight: "bold",
+                }}
+              >
+                Réserver maintenant!
+              </Button>
+            </div>
           </div>
         </div>
-
-        <div className="lg:w-1/2 md:w-1/2">
-          {/* <h1 className="lg:text-5xl md:text-4xl  mt-56 mb-4">
-            <span className="text-white font-bold">
-              LE 1ER SERVICE DE TAXI EN ALGÈRIE
-            </span>
-          </h1>
-          <p className="lg:text-2xl md:text-1xl mb-4">
-            <span className="text-white font-bold">
-              {" "}
-              TAXI MEHDI entreprie pour vous offrir des services de transports a
-              moitié du prix et avec une qualité supérieure
-            </span>
-          </p>
-          <div className="flex items-center">
-            <button className="rounded px-10 py-3 text-white bg-violet-500 hover:bg-violet-600">
-              Naviguer dans notre univers
-            </button>
-          </div> */}
-        </div>
-
-        {/* <div className="container mx-auto flex flex-col my-auto align-middle h-full">
-          <motion.div
-            className=" my-auto  mx-auto lg:mx-20 w-10/12 lg:w-2/5"
-            // variants={fadeIn("down", 0.01)}
-            // initial="hidden"
-            // whileInView={"show"}
-            // viewport={{ once: false, amount: 1 }}
-          >
-            <h1 className="lg:text-5xl md:text-4xl  mt-16 mb-4">
-              <span className="text-white font-bold">
-                LE 1ER SERVICE DE TAXI EN ALGÈRIE
-              </span>
-            </h1>
-            <p className="lg:text-2xl md:text-1xl mb-4">
-              <span className="text-white font-bold">
-                {" "}
-                TAXI MEHDI fondée en 2015, est créditée d’un capital expérience
-                au savoir-faire dans le domaine de l'intégration de solutions SI
-                reconnus et apprécié par nos partenaires métier.
-              </span>
-            </p>
-            <div className="flex items-center">
-              <button className="rounded px-10 py-3 text-white bg-violet-500 hover:bg-violet-600">
-                Naviguer dans notre univers
-              </button>
-            </div>
-          </motion.div>
-        </div> */}
       </div>
     </div>
   );
