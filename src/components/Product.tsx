026d9e00d363301518f039704eee6a42443ce6ea
Kebabi Abdelmounaim <kebabiabdel@gmail.com>
2024-06-07 14:31:14 +0100

    test

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -65,8 +65,8 @@ const Product = () => {
       </div>
       <div className=" px-8 mt-14 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12 ">
         <>
-          <div className="card px-4 py-6   md:h-100  shadow  flex h-full bg-white rounded-lg ">
-            <div className="mb-4 h-50 md:px-4 px-1   flex flex-col ">
+          <div className="card px-2 py-6   md:h-100  shadow  flex h-full bg-white rounded-lg ">
+            <div className="mb-4 h-50 md:px-4    flex flex-col ">
               <div
                 className={`text-center p-1 h-8 w-20 bg-bg1 rounded-md mb-8 text-bgWhite font-semibold`}
               >
@@ -94,8 +94,8 @@ const Product = () => {
           </div>
         </>
         <>
-          <div className="card px-4 py-6   md:h-100  shadow  flex h-full bg-white rounded-lg ">
-            <div className="mb-4 h-50 md:px-4 px-1  mx-auto flex flex-col ">
+          <div className="card px-2 py-6   md:h-100  shadow  flex h-full bg-white rounded-lg ">
+            <div className="mb-4 h-50 md:px-4  flex flex-col ">
               <div
                 className={`text-center p-1 h-8 w-20 bg-bg2 rounded-md mb-8 text-bgGreen font-semibold`}
               >
@@ -123,8 +123,8 @@ const Product = () => {
           </div>
         </>
         <>
-          <div className="card px-4 py-6   md:h-100  shadow   flex h-full bg-white rounded-lg ">
-            <div className="mb-4 h-50 md:px-4 px-1  mx-auto flex flex-col ">
+          <div className="card px-2 py-6   md:h-100  shadow   flex h-full bg-white rounded-lg ">
+            <div className="mb-4 h-50 md:px-4    flex flex-col ">
               <div
                 className={`text-center p-1 h-8 w-20 bg-bg3 rounded-md mb-8 text-bgWhite font-semibold`}
               >
