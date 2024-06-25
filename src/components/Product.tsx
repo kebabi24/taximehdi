3d98166532822ec22419020f6bc3f6397282eed1
Kebabi Abdelmounaim <kebabiabdel@gmail.com>
2024-06-25 19:53:06 +0100

    animationphone

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -73,129 +73,127 @@ const Product = () => {
   };
 
   return (
-    <InView onChange={(inView, entry) => setInsideView(inView)}>
-      {({ inView, ref, entry }) => (
-        <div
-          className="md:px-14  py-16  md:mr-16 md:ml-16 md:mt-16 md:mb-16 mr-5 ml-5 mt-5 mb-5  bg-bgB overflow-hidden rounded-2xl  "
-          id="solutions"
-          ref={ref}
-        >
-          <div className="text-center ">
-            <h1 className="font-outfit text-4xl text-textPrimary font-bold">
-              NOS
-            </h1>
-            <h1 className="font-outfit text-4xl text-bgWhite font-bold mb-10 ">
-              OFFRES
-            </h1>
-          </div>
-          {inView && (
-            <motion.ul
-              variants={container}
-              initial="hidden"
-              animate="visible"
-              className="container md:pl-36 md:pr-36 px-4 mt-14 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12 "
-            >
-              <>
-                <motion.li
-                  className="item card px-2 py-6   md:h-100  shadow  flex h-full bg-white rounded-lg "
-                  variants={item}
-                >
-                  <div className="mb-4 h-50 md:px-4    flex flex-col ">
-                    <div
-                      className={`text-center p-1 h-8 w-20 bg-bg1 rounded-md mb-8 text-bgWhite font-semibold`}
-                    >
-                      <p>Simple</p>
-                    </div>
-                    <div
-                      className={`flex flex-col p-1 h-10 w-56  rounded-md text-2xl  text-black font-bold`}
-                    >
-                      <p>≈ 200 DZD</p>
-                    </div>
-                    <p className="font-outfit text-xs text-grey mb-8 font-opensans ">
-                      Pas de crédite carte obligatoire
-                    </p>
-                    <Divider sx={{ minWidth: "100%", marginBottom: 3 }} />
-                    <div className="flex w-full p-1 mb-2 ">
-                      <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
-                      <p>Des chauffeurs ponctuelles & a l'heure</p>
-                    </div>
-
-                    <div className="flex w-full p-1 ">
-                      <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
-                      <p>Des chauffeurs ponctuelles & a l'heure</p>
-                    </div>
+    <div
+      className="md:px-14  py-16  md:mr-16 md:ml-16 md:mt-16 md:mb-16 mr-5 ml-5 mt-5 mb-5  bg-bgB overflow-hidden rounded-2xl  "
+      id="solutions"
+    >
+      <div className="text-center ">
+        <h1 className="font-outfit text-4xl text-textPrimary font-bold">NOS</h1>
+        <h1 className="font-outfit text-4xl text-bgWhite font-bold mb-10 ">
+          OFFRES
+        </h1>
+      </div>
+      <InView onChange={(inView, entry) => setInsideView(inView)}>
+        {({ inView, ref, entry }) => (
+          <motion.ul
+            variants={container}
+            initial="hidden"
+            animate="visible"
+            ref={ref}
+            className="container md:pl-36 md:pr-36 px-4 mt-14 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12 "
+          >
+            {inView && (
+              <motion.li
+                className="item card px-2 py-6   md:h-100  shadow  flex h-full bg-white rounded-lg "
+                variants={item}
+              >
+                <div className="mb-4 h-50 md:px-4    flex flex-col ">
+                  <div
+                    className={`text-center p-1 h-8 w-20 bg-bg1 rounded-md mb-8 text-bgWhite font-semibold`}
+                  >
+                    <p>Simple</p>
                   </div>
-                </motion.li>
-              </>
-              <>
-                <motion.li
-                  className="item card px-2 py-6   md:h-100  shadow  flex h-full bg-white rounded-lg "
-                  variants={item}
-                >
-                  <div className="mb-4 h-50 md:px-4  flex flex-col ">
-                    <div
-                      className={`text-center p-1 h-8 w-20 bg-bg2 rounded-md mb-8 text-bgGreen font-semibold`}
-                    >
-                      <p>Standard</p>
-                    </div>
-                    <div
-                      className={`flex flex-col p-1 h-10 w-56  rounded-md  text-2xl  text-black font-bold`}
-                    >
-                      <p>≈ 200 DZD</p>
-                    </div>
-                    <p className="font-outfit text-xs text-grey mb-8 font-opensans ">
-                      Pas de crédite carte obligatoire
-                    </p>
-                    <Divider sx={{ minWidth: "100%", marginBottom: 2 }} />
-                    <div className="flex w-full p-1 mb-2 ">
-                      <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
-                      <p>Des chauffeurs ponctuelles & a l'heure</p>
-                    </div>
-
-                    <div className="flex w-full p-1 ">
-                      <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
-                      <p>Des chauffeurs ponctuelles & a l'heure</p>
-                    </div>
+                  <div
+                    className={`flex flex-col p-1 h-10 w-56  rounded-md text-2xl  text-black font-bold`}
+                  >
+                    <p>≈ 200 DZD</p>
                   </div>
-                </motion.li>
-              </>
-              <>
-                <motion.li
-                  className="item card px-2 py-6   md:h-100  shadow   flex h-full bg-white rounded-lg "
-                  variants={item}
-                >
-                  <div className="mb-4 h-50 md:px-4    flex flex-col ">
-                    <div
-                      className={`text-center p-1 h-8 w-20 bg-bg3 rounded-md mb-8 text-bgWhite font-semibold`}
-                    >
-                      <p>Confort</p>
-                    </div>
-                    <div
-                      className={`flex flex-col p-1 h-10 w-56  rounded-md  text-2xl  text-black font-bold`}
-                    >
-                      <p>≈ 200 DZD</p>
-                    </div>
-                    <p className="font-outfit text-xs text-grey mb-8 font-opensans ">
-                      Pas de crédite carte obligatoire
-                    </p>
-                    <Divider sx={{ minWidth: "100%", marginBottom: 2 }} />
-                    <div className="flex w-full p-1 mb-2 ">
-                      <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
-                      <p>Des chauffeurs ponctuelles & a l'heure</p>
-                    </div>
-
-                    <div className="flex w-full p-1 ">
-                      <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
-                      <p>Des chauffeurs ponctuelles & a l'heure</p>
-                    </div>
+                  <p className="font-outfit text-xs text-grey mb-8 font-opensans ">
+                    Pas de crédite carte obligatoire
+                  </p>
+                  <Divider sx={{ minWidth: "100%", marginBottom: 3 }} />
+                  <div className="flex w-full p-1 mb-2 ">
+                    <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
+                    <p>Des chauffeurs ponctuelles & a l'heure</p>
                   </div>
-                </motion.li>
-              </>
-            </motion.ul>
-          )}
-        </div>
-      )}
-    </InView>
+
+                  <div className="flex w-full p-1 ">
+                    <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
+                    <p>Des chauffeurs ponctuelles & a l'heure</p>
+                  </div>
+                </div>
+              </motion.li>
+            )}
+
+            {inView && (
+              <motion.li
+                className="item card px-2 py-6   md:h-100  shadow  flex h-full bg-white rounded-lg "
+                variants={item}
+              >
+                <div className="mb-4 h-50 md:px-4  flex flex-col ">
+                  <div
+                    className={`text-center p-1 h-8 w-20 bg-bg2 rounded-md mb-8 text-bgGreen font-semibold`}
+                  >
+                    <p>Standard</p>
+                  </div>
+                  <div
+                    className={`flex flex-col p-1 h-10 w-56  rounded-md  text-2xl  text-black font-bold`}
+                  >
+                    <p>≈ 200 DZD</p>
+                  </div>
+                  <p className="font-outfit text-xs text-grey mb-8 font-opensans ">
+                    Pas de crédite carte obligatoire
+                  </p>
+                  <Divider sx={{ minWidth: "100%", marginBottom: 2 }} />
+                  <div className="flex w-full p-1 mb-2 ">
+                    <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
+                    <p>Des chauffeurs ponctuelles & a l'heure</p>
+                  </div>
+
+                  <div className="flex w-full p-1 ">
+                    <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
+                    <p>Des chauffeurs ponctuelles & a l'heure</p>
+                  </div>
+                </div>
+              </motion.li>
+            )}
+
+            {inView && (
+              <motion.li
+                className="item card px-2 py-6   md:h-100  shadow   flex h-full bg-white rounded-lg "
+                variants={item}
+              >
+                <div className="mb-4 h-50 md:px-4    flex flex-col ">
+                  <div
+                    className={`text-center p-1 h-8 w-20 bg-bg3 rounded-md mb-8 text-bgWhite font-semibold`}
+                  >
+                    <p>Confort</p>
+                  </div>
+                  <div
+                    className={`flex flex-col p-1 h-10 w-56  rounded-md  text-2xl  text-black font-bold`}
+                  >
+                    <p>≈ 200 DZD</p>
+                  </div>
+                  <p className="font-outfit text-xs text-grey mb-8 font-opensans ">
+                    Pas de crédite carte obligatoire
+                  </p>
+                  <Divider sx={{ minWidth: "100%", marginBottom: 2 }} />
+                  <div className="flex w-full p-1 mb-2 ">
+                    <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
+                    <p>Des chauffeurs ponctuelles & a l'heure</p>
+                  </div>
+
+                  <div className="flex w-full p-1 ">
+                    <IoIosSpeedometer size={24} style={{ marginRight: 15 }} />
+                    <p>Des chauffeurs ponctuelles & a l'heure</p>
+                  </div>
+                </div>
+              </motion.li>
+            )}
+          </motion.ul>
+        )}
+      </InView>
+    </div>
   );
 };
 
